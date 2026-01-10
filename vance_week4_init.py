c98f3da9876b2d0b807101061b5fa0e16284e65b
Gabriel Vance <ilovenarwhales@gmail.com>
2026-01-10 15:05:33 -0600

    Add initial implementation of sentence generator using PySpark

---
+++ vance_week4_init.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+import random
+
+# Start Spark
+spark = SparkSession.builder.appName("SentenceGenerator").getOrCreate()
+sc = spark.sparkContext
+
+# Word list
+words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"]
+
+# Generate sentences on the driver
+num_sentences = 1000
+sentences = [
+    " ".join(random.sample(words, random.randint(1, 6))) + "."
+    for _ in range(num_sentences)
+]
+
+# Parallelize into RDD
+sentences_rdd = sc.parallelize(sentences)
+
+# Transformation: add features to each sentence
+# this turns each sentence into a tuple of (sentence, total_words, unique_words)
+transformed = sentences_rdd.map(
+    lambda s: (
+        s,
+        len(s.replace(".", "").split()),                 # total words
+        len(set(s.replace(".", "").split()))             # unique words
+    )
+)
+
+
+# Show some results (will go to YARN driver logs in cluster mode)
+for line in transformed.take(100):
+    print(line)
+
+spark.stop()
